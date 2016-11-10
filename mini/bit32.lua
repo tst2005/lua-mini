d6c3bcfee4c203df0f8c07b0428266cae8ffa90b
TsT <tst2005@gmail.com>
2016-11-10 21:09:04 +0100

    improved mini.bit32

--- mini/bit32.lua
+++ mini/bit32.lua
@@ -3,8 +3,14 @@
 -- for old lua => posix.bit32 (removed from https://github.com/luaposix/luaposix/pull/206/commits/a7bdfab414b9d633b18bf6368c8e66dcb684b70c )
 -- http://bitop.luajit.org/api.html
 
-local function softrequire(name)
-	local ok, mod = pcall(require, name)
-	return ok and type(mod)=="table" and mod or nil
+local function requireany(names)
+	for _i, name in ipairs(names) do
+		local ok, mod = pcall(require, name)
+		print(name, ok)
+		if ok and type(mod)=="table" then
+			return mod
+		end
+	end
+	return nil
 end
-return softrequire "bitop" or softrequire "bit" or softrequire "bit32" or softrequire "mini.soft.bit32-forlua53" or nil
+return requireany {"bitop", "bit", "bit32", "mini.soft.bit.bit-lua53"}
