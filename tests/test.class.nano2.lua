089a57555f8859e1a2665c600db4bfff4ce3f2e3
TsT <tst2005@gmail.com>
2016-11-19 03:00:31 +0100

    missin test for nano class 2nd try

---
+++ tests/test.class.nano2.lua
@@ -0,0 +1,42 @@
+local class = require "mini.class.nano2"
+local instance = assert(class.instance)
+
+local foo = class("foo", nil, nil)
+function foo:hello() return "hello" end
+
+local bar = class("bar", nil, foo)
+function bar:world() return "world" end
+function bar:hello() return "hayllo "..self:world() end
+
+assert(foo.hello and foo:hello()=="hello")
+assert(foo.world==nil)
+assert(bar.hello and bar:hello()=="hayllo world")
+
+
+local foo1 = assert( instance(foo) )
+assert(foo1:hello()=="hello")
+
+local bar1 = assert( instance(bar) )
+assert(bar1:hello()=="hayllo world")
+
+--assert(foo._name == "foo" and bar._name == "bar")
+--print(foo1._name == nil)
+--print(bar1._name == nil)
+
+------------------------------------------------------------------
+
+local echo_class = class("echo", nil, nil)
+function echo_class:init(what)
+	self.what = assert(what)
+	return self
+end
+function echo_class:hello()
+	return self.what
+end
+
+local echo1 = assert( instance(echo_class, "la la la la") )
+local echo2 = assert( instance(echo_class, "pom pom pom") )
+assert(echo1:hello()=="la la la la")
+assert(echo2:hello()=="pom pom pom")
+
+print("OK")
