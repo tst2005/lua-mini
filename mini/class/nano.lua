0fed488ec406a8723214e6e48c6c4dfd443f6102
TsT <tst2005@gmail.com>
2016-11-19 01:37:56 +0100

    nano class renaming

--- mini/class/nano.lua
+++ mini/class/nano.lua
@@ -7,7 +7,7 @@
 
  * use <class>:newinstance(<args>) to create an new instance of <class>, do not call the class!
  * <meta>:__call meta is free for custom use
- * the new instance constructor is named "__newinstance", set up it on <class>:__newinstance(...)
+ * the new instance constructor is named "init", set up it on <class>:init(...)
  * the default built-in constructor return the new created instance
  * a custom constructor controls the returned instance, it will be usually end by a `return self` 
  * the class name is put in the <class>._name
@@ -28,36 +28,32 @@ local M = {}
 -- MIT License : https://github.com/airstruck/knife/blob/master/license
 local base = {
 	extend = function(self, subtype)
-		subtype = subtype or {}
-		local meta = { __index = subtype }
+		subtype = subtype or {}				-- class table
+		local meta = { __index = subtype }		-- instance metatable
 		assert(not subtype.newinstance)
 		subtype.newinstance = function(_self, ...)
 			local instance = setmetatable({}, meta)
-			return instance:__newinstance(...)
+			return instance:init(...)
 		end
 		return setmetatable(subtype, {__index = self})
 	end,
-	__newinstance = function(self) return self end,
+	init = function(self) return self end,
 }
 -- </thirdparty>
 
-local function newclass(name, prototype, parent)
+local function common_class(name, prototype, parent)
 	local parent = parent or base:extend()
 	local klass = parent:extend(prototype)
-	klass.__newinstance = (prototype or {}).__newinstance or (parent or {}).__newinstance
+	klass.init = (prototype or {}).init or (parent or {}).init
 	--klass._name = name
 	return klass
 end
 
---[[
-local function newinstance(class, ...)
-        return class(...)
-end
-]]--
-
-M.newclass = newclass
+M.newclass	= common_class
+M.class		= common_class
+--M.instance	= function(c, ...) return c:newinstance(...) end
 return setmetatable(M, {
 	__call = function(_, ...)
-		return newclass(...)
+		return common_class(...)
 	end,
 })
