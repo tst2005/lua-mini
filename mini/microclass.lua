42e79c8ae556d3b96e34bfbf8c99c6d762526623
TsT <tst2005@gmail.com>
2017-02-23 21:29:41 +0100

    microclass

---
+++ mini/microclass.lua
@@ -0,0 +1,11 @@
+
+local function instance(c)
+	return setmetatable({}, {__index=c})
+end
+
+local function class(prototype, parent)
+	local mt = {__index=parent, __call=instance}
+	return setmetatable(prototype or {}, mt) --, mt
+end
+
+return class
