d5fbdf3a1aecef05a8ca0b097d170e36cbc1d47e
TsT <tst2005@gmail.com>
2017-04-12 23:05:28 +0200

    splitfilter

--- mini/string/splitfilter.lua
+++ mini/string/splitfilter.lua
@@ -2,22 +2,31 @@
 local G = {string={find=string.find, sub=string.sub}, type=type, table={insert=table.insert}}
 
 -- the strong's split function https://github.com/tst2005/strong/blob/master/strong.lua#L189-L205
-return function(self, pat, plain, max)
+return function(self, pat, plain, max, filter)
 	--self=type(self)=="table" and self[1] or self
 	assert(type(self)=="string")
 	assert(type(pat)=="string")
 	--plain
-	assert(not max or type(max)=="number")
+	assert(not max or type(max)=="number", "invalid max number")
+	filter = filter or function(v) return v end
+	assert(type(filter)=="function")
 	local tinsert = table.insert or function(t,v) t[#t+1]=v end
 	local find, sub = string.find, string.sub
 	local t = {}
 	while true do
 		local pos1, pos2 = find(self, pat, 1, plain or false)
 		if not pos1 or pos1 > pos2 or max and #t>=max then
-			tinsert(t, self)
-			return t
+			if max and #t>=max then
+				return t, filter(self, t, true)
+			else
+				tinsert(t, filter(self, t, nil))
+				return t
+			end
 		end
-		tinsert(t, sub(self, 1, pos1 - 1))
+		tinsert(t, filter(sub(self, 1, pos1 - 1), t))
 		self = sub(self, pos2 + 1)
 	end
 end
+
+-- https://github.com/amireh/lua_cliargs/blob/master/src/cliargs/utils/split.lua
+
