eb64794338d9c081391d688679b9bc4abf10b679
TsT <tst2005@gmail.com>
2017-05-17 11:11:10 +0200

    mini.table.uniq: inline option

--- mini/table/uniq.lua
+++ mini/table/uniq.lua
@@ -1,17 +1,24 @@
-return function(t)
+return function(t, inline)
+	inline = inline==nil and true or false -- inline=true by default
+
 	local new = {}
 	local last = nil
 	for i,v in ipairs(t) do
-		if not last or last~=v then
+		if last == nil or last~=v then
 			new[#new+1]=v
 			last=v
 		end
 	end
-	for i=1,#t do
-		t[i]=nil
+	if inline then
+		-- remove all i-indexes
+		for i=1,#t do
+			t[i]=nil
+		end
+		-- add all uniq i-index
+		for i,v in ipairs(new) do
+			t[i]=v
+		end
+		return t
 	end
-	for i,v in ipairs(new) do
-		t[i]=v
-	end
-	return t
+	retur new
 end
