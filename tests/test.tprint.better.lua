0222257a2f7227153051d26ad1451fd4fa0499aa
TsT <tst2005@gmail.com>
2017-07-06 14:33:50 +0200

    tprint: more tests

--- tests/test.tprint.better.lua
+++ tests/test.tprint.better.lua
@@ -1,5 +1,19 @@
 local tprint = require "mini.tprint.better"
+local inline=true
+assert(tprint({}, {inline=inline})=="{}")
+assert(tprint({"a"}, {inline=inline})== '{"a",}')
+assert(tprint({aa="AA"}, {inline=inline,kshort=true})=='{aa = "AA",}')
+assert(tprint({aa="AA"}, {inline=inline,kshort=false})=='{["aa"] = "AA",}')
+assert(tprint({"aa"}, {inline=inline,ishort=false})=='{[1] = "aa",}')
 
+inline=false
+assert(tprint({}, {inline=inline})=="{\n}")
+assert(tprint({"a"}, {inline=inline})=='{\n\t"a",\n}')
+assert(tprint({aa="AA"}, {inline=inline,kshort=true})=='{\n\taa = "AA",\n}')
+assert(tprint({aa="AA"}, {inline=inline,kshort=false})=='{\n\t["aa"] = "AA",\n}')
+assert(tprint({"aa"}, {inline=inline,ishort=false})=='{\n\t[1] = "aa",\n}')
+
+--[[
 local t = {}
 t.a = {t2 = t}
 t.b = "b"
@@ -7,3 +21,4 @@ t.n = 3.14
 t[1]=true
 
 print("t="..tprint(t))
+]]--
